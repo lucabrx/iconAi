ccfc99a94234134513e6a46b70b46097e7655550
Luka Brkovic <coding@lbrkovic.com>
2023-05-02 21:59:48 +0200

    tailwind styles and typo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,23 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "my-gray-400" : "#4E5C74",
+        "my-blue-50" : "#E6EDFE",
+        "my-blue-900" : "#052C8F",
+        "cta-hover-dark" : "#6790F9",
+        "cta-dark" : "#356CF8",
+        "cta-light" : "#094AF1",
+        "my-gray-500" : "#7E8FA9",
+        "my-gray-200" : "#BCC4D2",
+        "text-sec-dark" : "#BEBEBE",
+        "text-sec-light" : "#394456",
+        "bg-dark" : "#101318",
+        "bg-light" : "#F9FAFB"
+
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
