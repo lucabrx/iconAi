31123933c3bcd5bf1e2cf3eba41831de27576ebd
Luka Brkovic <lukabrkovic@proton.me>
2023-07-11 20:15:22 +0200

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { type FC } from 'react';
 
 const Footer: FC = ({}) => {
   return (
-<div className='flex items-center justify-center text-sm text-text-sec-light dark:text-text-sec-dark'> 
+<div className='hidden md:flex items-center justify-center text-sm text-text-sec-light dark:text-text-sec-dark'>
 <a href="https://github.com/lucabrx" className='hover:underline'> designed by   lucabrx</a>
 </div>
 )
